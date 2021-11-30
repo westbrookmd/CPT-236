3f1bd4dc14da072093f2bfc5e55f6aea97014ec9
westbrookmd <71563811+westbrookmd@users.noreply.github.com>
2021-11-29 19:35:39 -0500

    Shift and Validation Added
    Added a shift value to make use of the previous extra  credit completed on the encryption/decryption shift values. Added validation to prevent errors.

--- Week14/EncryptionApplication.java
+++ Week14/EncryptionApplication.java
@@ -2,10 +2,7 @@ import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.Tooltip;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
@@ -27,21 +24,22 @@ public class EncryptionApplication extends Application {
 
         // Create buttons and adding to the flowpane
         Button btEncrypt = new Button("Encrypt");
-        //flowPane.add(btEncrypt);
         Button btClear = new Button("Clear");
-        //flowPane.add(btClear);
+        TextField txtShift = new TextField();
+        txtShift.setMaxWidth(80);
+        txtShift.setPromptText("Shift: 1 - 26");
         Button btDecrypt = new Button("Decrypt");
-        //flowPane.add(btDecrypt);
-        //The book's way of adding nodes to a flowpane
-        flowPane.getChildren().addAll(btEncrypt, btClear, btDecrypt);
+        flowPane.getChildren().addAll(btEncrypt, txtShift, btClear, btDecrypt);
 
         // Placing the flowpane and nodes in the gridPane
         gridPane.add(new Label("Input Text:"), 0, 0);
-        TextArea txtInput = new TextArea("Input text here");
+        TextArea txtInput = new TextArea();
+        txtInput.setPromptText("Input text here");
         gridPane.add(txtInput, 0, 1);
         gridPane.add(flowPane, 0, 2);
         gridPane.add(new Label("Output Text"), 0, 3);
-        TextArea txtOutput = new TextArea("Output text here");
+        TextArea txtOutput = new TextArea();
+        txtOutput.setPromptText("Output text here");
         txtOutput.setEditable(false); //make it so users can't type into the output box, but can copy it
         gridPane.add(txtOutput, 0, 4);
 
@@ -53,19 +51,70 @@ public class EncryptionApplication extends Application {
 
         //create event handlers
         btEncrypt.setOnMouseClicked(e -> {
+            int shift = getShift(txtShift.getText()); //this return 1 if the text wasn't a number within 1-26
             //get the input text
             String textToEncrypt = txtInput.getText();
-            //using the static method to encrypt the string so that we don't have to create an object
-            String encryptedText = EncryptString.encryptString(textToEncrypt);
-            txtOutput.setText(encryptedText);
+            EncryptString encryptedText = new EncryptString(textToEncrypt);
+            // This method allows for specific shifts or random shifts if no integer is given
+            String output = encryptedText.encryptStringRandom(encryptedText.getString(), shift);
+            txtOutput.setText(output);
+
         });
         btClear.setOnMouseClicked(e -> txtInput.clear());
         btDecrypt.setOnMouseClicked(e -> {
+            int shift = getShift(txtShift.getText()); //this return 1 if the text wasn't a number within 1-26
             String textToDecrypt = txtInput.getText();
-            //using the static method to decrypt the string so that we don't have to create an object
-            String decryptedText = EncryptString.decryptString(textToDecrypt);
-            txtOutput.setText(decryptedText);
+            EncryptString decryptedText = new EncryptString(textToDecrypt);
+            String output = decryptedText.decryptStringRandom(decryptedText.getString(), shift);
+            txtOutput.setText(output);
         });
+    }
+
+    private boolean checkIfValid(String shiftText)
+    {
+        boolean isValid = false;
+        try{
+            int shiftValue = Integer.parseInt(shiftText);
+            if(shiftValue > 0 && shiftValue <= 26)
+            {
+                isValid = true;
+            }
+            else{
+                displayAlert();
+            }
+        }
+        catch(NumberFormatException e){
+            isValid = false;
+            //make sure the shift isn't the default blank value
+            if(!(shiftText.isBlank())) {
+                displayAlert();
+            }
+        }
+        return isValid;
+    }
+
+    private void displayAlert() {
+        //create alert and explain the problem
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle("Check Shift Value");
+        alert.setHeaderText("Shift Not Within 1 - 26");
+        alert.setContentText("Verify that the shift value is a number greater than zero and less than 27.");
+        alert.showAndWait();
+    }
+
+    public int getShift(String shiftText)  //always calls checkIfValid
+    {
+        int shift = 1;
+        if(checkIfValid(shiftText))
+        {
+            try{
+                shift = Integer.parseInt(shiftText);
+            }
+            catch(NumberFormatException e){
+
+            }
+        }
+        return shift;
     }
 
 }
